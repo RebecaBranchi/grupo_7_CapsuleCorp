7a33317a2415d1bc122904382de46832d256e0a2
synti <synti.bogado@gmail.com>
2022-01-27 18:55:51 -0300

    rutas

--- PAGINA WEB/controllers/mainController.js
+++ PAGINA WEB/controllers/mainController.js
@@ -6,15 +6,15 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 
 const getHome = (req,res)=>{
-    res.render("index",{products})
+    res.render("users/index",{products})
 };
 
 const getLogin = (req,res)=>{
-    res.render("login")
+    res.render("users/login")
 };
 
 const getRegister = (req,res)=>{
-    res.render("register")
+    res.render("users/register")
 }
 
 
