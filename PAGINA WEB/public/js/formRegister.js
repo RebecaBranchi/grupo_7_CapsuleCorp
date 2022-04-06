8d56553ca14e85a5031141c0ecc4ac3a21782225
LuchoCardozo <you@example.com>
2022-04-05 22:38:18 -0300

    correcion validacion register

--- PAGINA WEB/public/js/formRegister.js
+++ PAGINA WEB/public/js/formRegister.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', function(){
 
-
     let formRegister = document.querySelector('form');
 
     formRegister.addEventListener('submit', function(e){
@@ -8,22 +7,24 @@ window.addEventListener('load', function(){
 
         let inputFirstName = document.querySelector("#first_name") ;
 
-        if (inputFirstName.val== ' ') {
+        if (inputFirstName.value ==" ") {
+        
             errors.push('El campo NOMBRE no debe estar vacío');
         }
-        console.log(inputFirstName.value);
+
         let inputLastName = document.querySelector('#last_name') ;
 
-        if (inputLastName.value == '') {
+        if (inputLastName.value == " ") {
+     
             errors.push('El campo APELLIDO no debe estar vacío');
         }
 
         let inputEmail = document.querySelector('#email') ;
 
-        if (inputEmail.value == '') {
+        if (inputEmail.value == " ") {
             errors.push('El campo EMAIL no debe estar vacío');
         } else if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(inputEmail.value)){
-            errors.push("La dirección de email " + inputEmail.value + " es correcta!.");
+            alert("La dirección de email " + inputEmail.value + " es correcta!.");
            } else {
             errors.push("La dirección de email es incorrecta!.");
            }
@@ -31,12 +32,12 @@ window.addEventListener('load', function(){
 
         let inputPassword = document.querySelector('#password') ;
 
-        if (inputPassword.value == '') {
+        if (inputPassword.value == ' ') {
             errors.push('El campo CONTRASEÑA no debe estar vacío');
         } else if (inputPassword.value.length < 8) {
             errors.push('La contraseña debe tener al menos 8 caracteres')
         }
-        console.log(inputPassword.value);
+   
 
         if(errors.length > 0) {
             e.preventDefault();
