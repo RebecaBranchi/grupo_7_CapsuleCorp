098ca1e7605ede0acc6f7c8db1952162904aa825
LuchoCardozo <you@example.com>
2022-04-24 22:32:21 -0300

    arreglo de css, categorias y marcas

--- PAGINA WEB/middlewares/adminMiddleware.js
+++ PAGINA WEB/middlewares/adminMiddleware.js
@@ -1,8 +1,6 @@
 function adminMiddLeware(req, res, next) {
-
-    if (req.session.userLogged.category_id != 2) {
+    if (!req.session.userLogged || req.session.userLogged.category_id != 2) {
         return res.redirect('/');
-
     }
     next();
 }
--- PAGINA WEB/public/js/burger.js
+++ PAGINA WEB/public/js/burger.js
@@ -5,16 +5,16 @@ window.addEventListener('load', function() {
     let leftNavBar = document.querySelector('.left-navbar')
     let rightNavBar = document.querySelector('.right-navbar')
     let barra = document.querySelector('.sep')
+    let imgProfile = document.querySelector('.imgProfile')
 
     burger.addEventListener('click', () => {
         if (burger.style.display === 'block' && screen.width <= 768) {
             mainNavBar.classList.toggle('mainNavBar')
             leftNavBar.classList.remove('left-navbar')
             rightNavBar.classList.remove('right-navbar')
+            imgProfile.classList.toggle('imgProfile2')
             barra.classList.toggle('sep2')
         }
-
     })
-
 
 })
